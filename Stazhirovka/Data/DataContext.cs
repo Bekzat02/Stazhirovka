d852e142d5d9061803a5b4eaad94e37416205b94
Bekzat Akhmetov <b.akhmetov@astanait.edu.kz>
2020-10-28 02:57:10 +0600

    This project created special for Internship Nur-Sultan

---
+++ Stazhirovka/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stazhirovka.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stazhirovka.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<GenreReference> GenreReferences { get; set; }
+    }
+}
